9f21e00fbfe83bb8622a6d7057c0c3e8850ecee0
q1052780722 <71871574+q1052780722@users.noreply.github.com>
2022-08-16 16:34:22 +0800

    First commit demo

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+html, body {
+    overflow: hidden;
+    margin: 0;
+    font-size: 12px;
+}
+#app{
+  margin: 0;
+  padding: 0;
+}
+</style>
